71424a2ee0d3458fc4b0ad24d7809167228e5a05
Konrad Piegza <piegzini.k@gmail.com>
2021-05-29 15:10:08 +0200

    Drugi commit aplikacji irc

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { join } = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+let pools = [];
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -14,8 +15,28 @@ app.use(express.static(join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'public', 'html', 'index.html'));
 });
-app.get('/gowno', (require, request) => {
-  const log = 'dupa';
+app.get('/query', (require, response) => {
+  pools.push(response);
 });
 
+app.post('/message', (require, response) => {
+  const requireMessage = require.body.content;
+  const message = { content: requireMessage };
+  const emptyMessageValidator = requireMessage.split('').join('');
+
+  if (emptyMessageValidator) {
+    const responseMessage = JSON.stringify(message);
+    pools.forEach((poll) => {
+      try {
+        poll.send(responseMessage);
+      } catch (e) {
+        const error = new Error(e);
+      }
+    });
+    pools = [];
+    response.send(responseMessage);
+  } else {
+    response.end();
+  }
+});
 module.exports = app;
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,16 +1,46 @@
 // 5 second timeout:
 
-const sendQuery = async () => {
-  try {
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), 5000);
-    const response = await fetch('/gowno', { signal: controller.signal });
-    console.log('jest');
-  } catch (e) {
-    console.log(e);
-  } finally {
-    sendQuery();
-  }
-};
+class Chat {
+  constructor() {
+    this.input = document.querySelector('.irc-input');
+    this.chat = document.querySelector('.irc-chat');
 
-sendQuery();
+    document.addEventListener('keyup', sendMessage);
+    this.sendQuery();
+  }
+
+  async sendQuery() {
+    try {
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), 5000);
+      const responseMessage = await fetch('/query', { signal: controller.signal });
+      const { content } = await responseMessage.json();
+      this.chat.innerHTML += `<p>${content}</p>`;
+      this.chat.scrollTop = this.chat.scrollHeight;
+    } catch (e) {
+      const error = new Error(e);
+      console.log(error);
+    } finally {
+      this.sendQuery();
+    }
+  }
+
+  sendMessage = async (e) => {
+    const inputValue = input.value;
+    const emptyMessageValidator = inputValue.split('').join('');
+    const { key } = e;
+
+    if (key === 'Enter' && emptyMessageValidator) {
+      const message = { content: inputValue };
+      const parsedMessage = JSON.stringify(message);
+      const response = await fetch('/message', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: parsedMessage,
+      });
+      input.value = '';
+    }
+  };
+}
